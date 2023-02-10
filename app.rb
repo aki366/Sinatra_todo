856658d14407775fab6bad07b98de87c6a885077
aki <akihiro.okuda366@gmail.com>
2023-02-10 15:30:39 +0900

    URL から値を取得する

--- app.rb
+++ app.rb
@@ -5,8 +5,13 @@ get '/' do
   'hello!!'
   'http://localhost:4567/top.html'
   'http://localhost:4567/path/to'
+  "http://localhost:4567/[name].html"
 end
 
 get '/path/to' do
   "this is [/path/to]"
+end
+
+get '/hello/*' do |name|
+  "hello #{name}. how are you?"
 end
