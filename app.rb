eec71b4c22933f1fd2de454ac06b408d10c22a1b
aki <akihiro.okuda366@gmail.com>
2023-02-13 16:06:55 +0900

    私用しないルーティングとファイルを削除

--- app.rb
+++ app.rb
@@ -64,24 +64,3 @@ patch '/memos/:id' do
 
   redirect "/memos/#{params[:id]}"
 end
-
-get '/path/to' do
-  "this is [/path/to]"
-end
-
-get '/hello/*' do |name|
-  "hello #{name}. how are you?"
-end
-
-get '/erb_template_page' do
-  erb :erb_template_page
-end
-
-get '/markdown_template_page' do
-  markdown :markdown_template_page
-end
-
-# erbとMarkdownを組み合わせる
-get '/erb_and_md_template_page' do
-  erb :erb_and_md_template_page, :locals => { :md => markdown(:erb_and_md_template_page) }
-end
