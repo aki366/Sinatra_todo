23abfe37150f90e6565de896d48fa6b46012fb3d
aki <akihiro.okuda366@gmail.com>
2023-02-20 22:55:24 +0900

    FILE_PATHを定義することで引数を省略

--- app.rb
+++ app.rb
@@ -7,12 +7,12 @@ require 'cgi/escape'
 
 FILE_PATH = 'public/memos.json'
 
-def get_memos(file_path)
-  File.open(file_path) { |f| JSON.parse(f.read) }
+def get_memos
+  File.open(FILE_PATH) { |f| JSON.parse(f.read) }
 end
 
-def set_memos(file_path, memos)
-  File.open(file_path, 'w') { |f| JSON.dump(memos, f) }
+def set_memos(memos)
+  File.open(FILE_PATH, 'w') { |f| JSON.dump(memos, f) }
 end
 
 get '/' do
@@ -20,7 +20,7 @@ get '/' do
 end
 
 get '/memos' do
-  @memos = get_memos(FILE_PATH)
+  @memos = get_memos
   erb :index
 end
 
@@ -29,7 +29,7 @@ get '/memos/new' do
 end
 
 get '/memos/:id' do
-  memos = get_memos(FILE_PATH)
+  memos = get_memos
   @title = memos[params[:id]]['title']
   @content = memos[params[:id]]['content']
   erb :show
@@ -39,16 +39,16 @@ post '/memos' do
   title = params[:title]
   content = params[:content]
   if title != ''
-    memos = get_memos(FILE_PATH)
+    memos = get_memos
     id = (memos.keys.map(&:to_i).max + 1).to_s
     memos[id] = { 'title' => title, 'content' => content }
-    set_memos(FILE_PATH, memos)
+    set_memos(memos)
   end
   redirect '/memos'
 end
 
 get '/memos/:id/edit' do
-  memos = get_memos(FILE_PATH)
+  memos = get_memos
   @title = memos[params[:id]]['title']
   @content = memos[params[:id]]['content']
   erb :edit
@@ -58,17 +58,17 @@ patch '/memos/:id' do
   title = params[:title]
   content = params[:content]
   if title != ''
-    memos = get_memos(FILE_PATH)
+    memos = get_memos
     memos[params[:id]] = { 'title' => title, 'content' => content }
-    set_memos(FILE_PATH, memos)
+    set_memos(memos)
   end
   redirect "/memos/#{params[:id]}"
 end
 
 delete '/memos/:id' do
-  memos = get_memos(FILE_PATH)
+  memos = get_memos
   memos.delete(params[:id])
-  set_memos(FILE_PATH, memos)
+  set_memos(memos)
 
   redirect '/memos'
 end
